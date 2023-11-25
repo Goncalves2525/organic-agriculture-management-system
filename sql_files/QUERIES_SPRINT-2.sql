3a44147dd63c495844ea63ed6214290f55d5e1b5
Tassio Gomes <ds.tassio@gmail.com>
2023-11-25 12:24:55 +0000

    US BD15 and US BD20

--- sql_files/QUERIES_SPRINT-2.sql
+++ sql_files/QUERIES_SPRINT-2.sql
@@ -1,5 +1,43 @@
 -- SPRINT 2
 
+/**
+* US BD15
+*Como Gestor Agrícola, quero registar uma operação de poda
+*/
+
+CREATE OR REPLACE FUNCTION insert_poda(
+    p_idoperacao operacoes.idoperacao%TYPE,
+    p_quantidade NUMBER,
+    p_unidade VARCHAR2
+) RETURN VARCHAR2
+AS
+BEGIN
+    -- Insert into PODAS table
+    INSERT INTO PODAS VALUES(p_idoperacao, p_quantidade, p_unidade);
+
+    -- Return success message
+    RETURN 'Insert successful';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        -- Handle exceptions
+        RETURN 'Error: ' || SQLCODE || ' - ' || SQLERRM;
+END insert_poda;
+/
+
+
+-- chamar a funcao
+
+DECLARE
+    result_message VARCHAR2(200);
+BEGIN
+    result_message := insert_poda(:idoperacao, :quantidade, 'un');
+    DBMS_OUTPUT.PUT_LINE(result_message);
+END;
+/
+
+
+
 /*
 * USBD18
 * Como Gestor Agrícula,
@@ -129,6 +167,7 @@ GROUP BY
     OD.PARCELAID, OD.DATAiNICIO, OD.OperationType
 ORDER BY
     OD.PARCELAID, OD.OperationType;
+	
 
 
 /*
@@ -156,6 +195,83 @@ FROM aplicacoes_fatprod
 WHERE operacoes.dataInicio BETWEEN TO_DATE('2010-12-10  ', 'YYYY-MM-DD') AND TO_DATE('2030-01-20', 'YYYY-MM-DD')
 GROUP BY operacoes.idoperacao, aplicacoes_fatprod.fatorprodid, formulacoes.tipo, parcelas.idparcela, parcelas.nome, culturas.idcultura, culturas.nome
 ORDER BY operacoes.idoperacao;
+
+
+
+/**
+* US BD20
+*Como Gestor Agrícola, pretendo obter os totais de rega mensal de cada parcela, 
+*num dado intervalo de tempo.
+*/
+SELECT parcelas.idparcela, parcelas.nome, parcelas.area, parcelas.unidademedida,
+    EXTRACT(MONTH FROM operacoes.datainicio) AS mes,
+    EXTRACT(YEAR FROM operacoes.datainicio) AS ano,
+    SUM(REGAS.QUANTIDADE) AS TOTALDEREGAS 
+FROM QUINTAS
+JOIN PARCELAS ON (quintas.idquinta = parcelas.quintaid)
+JOIN OPERACOES ON (parcelas.idparcela = operacoes.parcelaid)
+JOIN REGAS ON (operacoes.idoperacao = regas.operacaoid)
+WHERE operacoes.datainicio BETWEEN TO_DATE('2010-12-10  ', 'YYYY-MM-DD') AND TO_DATE('2030-01-20', 'YYYY-MM-DD')
+GROUP BY parcelas.idparcela, parcelas.nome, parcelas.area, parcelas.unidademedida, 
+    EXTRACT(MONTH FROM operacoes.datainicio),
+    EXTRACT(YEAR FROM operacoes.datainicio)
+
+
+CREATE OR REPLACE PROCEDURE GetParcelasData(
+    p_datainicio VARCHAR2,
+    p_datafim VARCHAR2
+)
+IS
+BEGIN
+    FOR result IN (
+        SELECT 
+            parcelas.idparcela, 
+            parcelas.nome, 
+            parcelas.area, 
+            parcelas.unidademedida,
+            EXTRACT(MONTH FROM operacoes.datainicio) AS mes,
+            EXTRACT(YEAR FROM operacoes.datainicio) AS ano,
+            SUM(REGAS.QUANTIDADE) AS TOTALDEREGAS 
+        FROM 
+            QUINTAS
+        JOIN 
+            PARCELAS ON quintas.idquinta = parcelas.quintaid
+        JOIN 
+            OPERACOES ON parcelas.idparcela = operacoes.parcelaid
+        JOIN 
+            REGAS ON operacoes.idoperacao = regas.operacaoid
+        WHERE 
+            operacoes.datainicio BETWEEN TO_DATE('2010-12-10  ', 'YYYY-MM-DD') AND TO_DATE('2030-01-20', 'YYYY-MM-DD') AND TO_DATE('2010-12-10  ', 'YYYY-MM-DD') AND TO_DATE('2030-01-20', 'YYYY-MM-DD')
+        GROUP BY 
+            parcelas.idparcela, 
+            parcelas.nome, 
+            parcelas.area, 
+            parcelas.unidademedida, 
+            EXTRACT(MONTH FROM operacoes.datainicio),
+            EXTRACT(YEAR FROM operacoes.datainicio)
+    ) LOOP
+        -- Process the result
+        DBMS_OUTPUT.PUT_LINE(
+            'ID Parcela: ' || result.idparcela ||
+            ', Nome: ' || result.nome ||
+            ', Área: ' || result.area ||
+            ', Unidade de Medida: ' || result.unidademedida ||
+            ', Mês: ' || result.mes ||
+            ', Ano: ' || result.ano ||
+            ', Total de Regas: ' || result.TOTALDEREGAS
+        );
+    END LOOP;
+END GetParcelasData;
+/
+
+SET SERVEROUTPUT ON;
+DECLARE
+    datainicio VARCHAR2(10) := ('2010-12-10  ', 'YYYY-MM-DD');
+    datafim VARCHAR2(10) := TO_DATE('2030-01-20', 'YYYY-MM-DD');
+BEGIN
+    GetParcelasData(datainicio, datafim);
+END;
+/
 
 
 
